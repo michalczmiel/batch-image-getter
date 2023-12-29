0d5294ecc4772124160fc6c69a327ab49917fbdc
Michał Czmiel <michalczmiel@outlook.com>
2023-12-29 14:35:07 +0100

    feat: add website command

--- cmd/root.go
+++ cmd/root.go
@@ -1,68 +1,10 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-	"sync"
-
-	"github.com/michalczmiel/batch-image-downloader/internal"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	RunE: run,
-	Args: cobra.MinimumNArgs(1),
-}
-
-func init() {
-	rootCmd.Flags().StringArrayP("types", "t", []string{".jpg", ".jpeg", ".png"}, "image types to download")
-}
-
-func run(cmd *cobra.Command, args []string) error {
-	url := args[0]
-
-	imageTypesToDownload, err := cmd.Flags().GetStringArray("types")
-	if err != nil {
-		return err
-	}
-
-	doc, err := internal.GetHtmlDocFromUrl(url)
-	if err != nil {
-		return err
-	}
-
-	rawLinks := internal.GetImageLinksFromHtmlDoc(doc)
-	if len(rawLinks) == 0 {
-		return fmt.Errorf("no links found")
-	}
-
-	links := internal.ProcessLinks(url, rawLinks, imageTypesToDownload)
-
-	var wg sync.WaitGroup
-	for _, link := range links {
-		wg.Add(1)
-
-		go func(l string) {
-			defer wg.Done()
-
-			fileName := internal.GetFileNameFromUrl(l)
-			err = internal.DownloadFileFromUrl(l, fileName)
-			if err != nil {
-				fmt.Printf("Error downloading file %s %v", l, err)
-			}
-		}(link)
-	}
-
-	wg.Wait()
-
-	return nil
-}
-
-func Execute() {
-	err := rootCmd.Execute()
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	Use:   "big",
+	Short: "big is a CLI tool for downloading images",
 }
---
+++ cmd/website.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/michalczmiel/batch-image-downloader/internal"
+	"github.com/spf13/cobra"
+)
+
+var websiteCmd = &cobra.Command{
+	Use:   "website",
+	Short: "Download all images from a website",
+	RunE:  run,
+	Args:  cobra.MinimumNArgs(1),
+}
+
+func init() {
+	websiteCmd.Flags().StringArrayP("types", "t", []string{".jpg", ".jpeg", ".png"}, "image types to download")
+	rootCmd.AddCommand(websiteCmd)
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	url := args[0]
+
+	imageTypesToDownload, err := cmd.Flags().GetStringArray("types")
+	if err != nil {
+		return err
+	}
+
+	doc, err := internal.GetHtmlDocFromUrl(url)
+	if err != nil {
+		return err
+	}
+
+	rawLinks := internal.GetImageLinksFromHtmlDoc(doc)
+	if len(rawLinks) == 0 {
+		return fmt.Errorf("no links found")
+	}
+
+	links := internal.ProcessLinks(url, rawLinks, imageTypesToDownload)
+
+	var wg sync.WaitGroup
+	for _, link := range links {
+		wg.Add(1)
+
+		go func(l string) {
+			defer wg.Done()
+
+			fileName := internal.GetFileNameFromUrl(l)
+			err = internal.DownloadFileFromUrl(l, fileName)
+			if err != nil {
+				fmt.Printf("Error downloading file %s %v", l, err)
+			}
+		}(link)
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
