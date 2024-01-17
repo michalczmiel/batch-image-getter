a4c77ae958768e0d1413cd125ab94f9602226f6f
Michał Czmiel <michalczmiel@outlook.com>
2024-01-17 18:40:36 +0100

    refactor: move file related utils

--- internal/files.go
+++ internal/files.go
@@ -3,6 +3,8 @@ package internal
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 const DefaultPath = "."
@@ -22,4 +24,32 @@ func CreateDirectoryIfDoesNotExists(directory string) error {
 	}
 
 	return nil
+}
+
+func validateContentType(contentType string, imageTypes []string) error {
+	if !strings.HasPrefix(contentType, "image") {
+		return fmt.Errorf("content type '%s' is not an image", contentType)
+	}
+
+	imageType := strings.Split(contentType, "/")[1]
+
+	for _, allowedImageType := range imageTypes {
+		if imageType == allowedImageType {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("image type '%s' is not allowed", imageType)
+}
+
+func addExtensionIfMissing(filePath, contentType string) string {
+	extension := filepath.Ext(filePath)
+
+	if extension != "" {
+		return filePath
+	}
+
+	extension = "." + strings.Split(contentType, "/")[1]
+
+	return filePath + extension
 }
--- internal/http.go
+++ internal/http.go
@@ -6,8 +6,6 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
 	"golang.org/x/net/html"
@@ -57,34 +55,6 @@ func request(url, userAgent string) (*http.Response, error) {
 	}
 
 	return response, nil
-}
-
-func validateContentType(contentType string, imageTypes []string) error {
-	if !strings.HasPrefix(contentType, "image") {
-		return fmt.Errorf("content type '%s' is not an image", contentType)
-	}
-
-	imageType := strings.Split(contentType, "/")[1]
-
-	for _, allowedImageType := range imageTypes {
-		if imageType == allowedImageType {
-			return nil
-		}
-	}
-
-	return fmt.Errorf("image type '%s' is not allowed", imageType)
-}
-
-func addExtensionIfMissing(filePath, contentType string) string {
-	extension := filepath.Ext(filePath)
-
-	if extension != "" {
-		return filePath
-	}
-
-	extension = "." + strings.Split(contentType, "/")[1]
-
-	return filePath + extension
 }
 
 func DownloadImageFromUrl(url, filePath string, parameters Parameters) error {
